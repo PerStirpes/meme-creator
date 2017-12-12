51731a7d905b12d54c5a011d79efedcc50d5685a
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-12-12 05:09:47 -0800

    update app.js prettier

--- server/app.js
+++ server/app.js
@@ -1,59 +1,65 @@
-const express = require("express");
-const app = express();
-const cors = require('cors');
-const request = require("request");
-const bodyParser = require("body-parser");
-const userRoutes = require("./routes/users");
-const authRoutes = require("./routes/auth");
-const memeRoutes = require("./routes/memes");
-const authMiddleware = require("./middleware/auth");
-const db = require("./models");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const request = require('request')
+const bodyParser = require('body-parser')
+const userRoutes = require('./routes/users')
+const authRoutes = require('./routes/auth')
+const memeRoutes = require('./routes/memes')
+const authMiddleware = require('./middleware/auth')
+const db = require('./models')
 
 if (process.env.NODE_ENV !== 'production') {
-  require("dotenv").config()
+  require('dotenv').config()
 }
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get("/", (req,res) => {
-  res.send("start with /api/users");
-});
+app.get('/', (req, res) => {
+  res.send('start with /api/users')
+})
 
-app.use('/api/users/:id/memes', authMiddleware.loginRequired,
-        authMiddleware.ensureCorrectUser, memeRoutes);
-app.use('/api/users', authMiddleware.loginRequired, userRoutes);
+app.use(
+  '/api/users/:id/memes',
+  authMiddleware.loginRequired,
+  authMiddleware.ensureCorrectUser,
+  memeRoutes
+)
+app.use('/api/users', authMiddleware.loginRequired, userRoutes)
 
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes)
 
-app.get('/memes/options', (req,res) => {
-  request.get("https://api.imgflip.com/get_memes",
-    function(error, response, body) {
-      if (error) {
-        next(error);
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).send(body);
+app.get('/memes/options', (req, res) => {
+  request.get('https://api.imgflip.com/get_memes', function(
+    error,
+    response,
+    body
+  ) {
+    if (error) {
+      next(error)
     }
-  );
-});
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(body)
+  })
+})
 
 app.get('/memes', (req, res, next) => {
   db.Meme.find({}, (err, memes) => {
     if (err) {
-      next(err);
+      next(err)
     } else {
-      res.status(200).send(memes);
+      res.status(200).send(memes)
     }
-
-  });
-});
+  })
+})
 
 app.use((err, req, res, next) => {
-  res.send(err);
-});
+  res.send(err)
+})
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+app.listen(4200, err => {
+  if (err) throw err
+  console.log('server listening on *:4200')
+})
